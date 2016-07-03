f4691b232dc4f2c8029c4ad74e906ed0cecf4016
MrLokans <trikster1911@gmail.com>
2016-07-03 11:20:03 +0300

    Now workers are not launched from the root user.

--- parser_routines/tasks.py
+++ parser_routines/tasks.py
@@ -1,7 +1,4 @@
-from datetime import timedelta
-
 from celery import Celery
-from celery.decorators import periodic_task
 
 celery = Celery('tasks')
 celery.config_from_object('celeryconfig')
@@ -12,11 +9,3 @@ def add(x, y):
     with open("test.txt", "a") as f:
         f.write("TEST\n")
     return x + y
-
-
-@periodic_task(run_every=timedelta(seconds=10),
-               name="test_periodic_task")
-def add():
-    with open("test.txt", "a") as f:
-        f.write("TEST\n")
-    return "RAN"
