d517521bf123dbde41f379438e25199babdda3c0
MrLokans <trikster1911@gmail.com>
2016-07-13 18:29:07 +0300

    #14. Now front-end obtains street data from API.

--- nginx_microservice/static/js/input_helper.js
+++ nginx_microservice/static/js/input_helper.js
@@ -2,7 +2,6 @@ $(document).ready(function($){
     var cityInput = $('#city-input');
     var streetInput = $('#street-input');
 
-
     var cityDatalist = $('#city-datalist');
     var streetDatalist = $('#street-datalist');
 
@@ -26,4 +25,27 @@ $(document).ready(function($){
             }
         );
     });
+
+    streetInput.on("keyup", function(e){
+        // process alphanumerical characters only  
+        if ( !(e.which >= 48  && e.which <= 90) )  {
+             return;
+        }
+        street = $(this).val();
+        city = cityInput.val();
+        var ajax_url = '/api/streets?city=' + city + '&street_query=' + street;
+        $.ajax(ajax_url,
+            {
+                crossDomain: true,
+                success: function(result){
+                    streetDatalist.empty();
+                    result.streets.forEach(function(item, i, arr){
+                        newOption = $('<option value=' + item + '>');
+                        streetDatalist.append(newOption);
+                    });
+                }
+            }
+        );
+    });
+
 });
