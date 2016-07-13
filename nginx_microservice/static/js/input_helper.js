bb58595d91c26020454e41b5ef0027c737529c9e
MrLokans <trikster1911@gmail.com>
2016-07-13 19:17:48 +0300

    #14. Dummy method to render connections added. Mustache library addeds as bower's dependency.

--- nginx_microservice/static/js/input_helper.js
+++ nginx_microservice/static/js/input_helper.js
@@ -1,9 +1,27 @@
+var render_row = function(city, street, house, provider, provider_url, status){
+    data = {
+        city: city,
+        street: street,
+        house: house,
+        provider: provider,
+        url: provider_url,
+        status: status 
+    };
+    var template = $('#connection-row-template').html();
+    Mustache.parse(template);   // optional, speeds up future uses
+    return Mustache.render(template, data);
+
+};
+
 $(document).ready(function($){
     var cityInput = $('#city-input');
     var streetInput = $('#street-input');
 
     var cityDatalist = $('#city-datalist');
     var streetDatalist = $('#street-datalist');
+
+    var searchButton = $('#search-sumbit');
+    var connectionsList = $('#connections-list');
 
     cityInput.on("keyup", function(e){
         // process alphanumerical characters only  
@@ -46,6 +64,22 @@ $(document).ready(function($){
                 }
             }
         );
+    });
+
+    searchButton.on("click", function(e){
+        e.preventDefault();
+        $('#connections-list .connection-row').remove();
+    
+        defaultData = {"connections": [
+            {"city": "Минск", "street": "Карла Либнехта", "house": "98", "url": "http://byfly.by", "provider": "byfly", "status": "Хуй вам, а не XPON"},
+            {"city": "Минск", "street": "Карла Либнехта", "house": "94", "url": "http://byfly.by", "provider": "byfly", "status": "Хуй вам, а не XPON"},
+            {"city": "Минск", "street": "Карла Либнехта", "house": "96", "url": "http://byfly.by", "provider": "byfly", "status": "Хуй вам, а не XPON"}
+                              ]};
+      // city, street, house, provider, provider_url, status
+        defaultData.connections.forEach(function(item, i, arr){
+            var newRow = render_row(item.city, item.street, item.house, item.provider, item.url, item.status);
+            connectionsList.append($(newRow));
+        });
     });
 
 });
